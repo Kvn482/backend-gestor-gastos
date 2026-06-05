010171592bd0e1f33031ecde969d93f4120042cb
KvnAgromaster <dev1@agromaster.com.mx>
2026-06-05 15:43:13 -0700

    Add DNS configuration to prioritize IPv4 addresses

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -8,6 +8,9 @@ const nodemailer = require('nodemailer');
 //   },
 // });
 
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
